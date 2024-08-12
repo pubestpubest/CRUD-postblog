3a2640bf243dccf2f43cdb39d52e28e6c9de0171
pubestpubest <pubestpubest@gmail.com>
2024-08-12 21:49:30 +0700

    fixed posts get

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,11 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export function GET() {
-  return Response.json({
-    title: "hello",
-    content: "prisma",
-  });
+export async function GET() {
+  return Response.json(await prisma.post.findMany());
 }
 
 export async function POST(req: Request) {
